55ac4d523ef25437203e5128d3cda1fa2f620b55
William Barela <william.barela.dev@gmail.com>
2020-05-19 22:45:33 -0500

    add method to shuffle arrays

--- src/js/Display.js
+++ src/js/Display.js
@@ -22,4 +22,13 @@ export class Display {
               console.log(json);
             });
   }
+
+  // randomly shuffles an array in n time
+  static shuffle(arr) => {
+    for (let i = arr.length - 1; i >= 0; i--){
+        const j = Math.floor(Math.random() * i);
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
 }
