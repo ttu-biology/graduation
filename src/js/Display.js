2e393fc52f5aca93b59b4b80c67a281aed6a3725
William Barela <william.barela.dev@gmail.com>
2020-05-19 23:57:53 -0500

    add methods to build image tag and collect image tags

--- src/js/Display.js
+++ src/js/Display.js
@@ -1,30 +1,47 @@
 export class Display {
-  constructor(imageIndex, baseUrl='http://www.depts.ttu.edu/biology/graduation'){
-    this.imageIndex = imageIndex;
+  constructor(imgIndex, baseUrl='http://www.depts.ttu.edu/biology/graduation'){
+    this.imgIndex = imgIndex;
+    this.imgPath = "src/img";
     this.baseUrl = baseUrl;
   }
 
   get load() {
     window.addEventListener('DOMContentLoaded', (e) => {
-      this.imageList;
+      this.getImgList;
     });
   }
 
-  buildImageGrid(json) {
+  buildImgTag(filename) {
+    const imgTag = document.createElement('img');
+    imgTag.src = `${this.imgPath}/${filename}`;
 
+    return imgTag;
+  }
+  
+  buildImgTags(json) {
+    const [portraitImg, landscapeImg] = [Display.shuffle(json.portrait), Display.shuffle(json.landscape)];
+    const portraitImgTags = portraitImg.map((filename) => this.buildImgTag(filename));
+    const landscapeImgTags = landscapeImg.map((filename) => this.buildImgTag(filename));
+
+    return [portraitImgTags, landscapeImgTags];
   }
 
-  get imageList(){
-    return fetch(`${this.baseUrl}/src/img/${this.imageIndex}`)
+  buildImgGrid(json) {
+    const [portraitImgTags, landscapeImgTags] = this.buildImgTags(json);
+    console.table(portraitImgTags.map((tag) => tag.src));
+    console.table(landscapeImgTags.map((tag) => tag.src));
+  }
+
+  get getImgList(){
+    return fetch(`${this.baseUrl}/src/img/${this.imgIndex}`)
             .then(response => response.json())
             .then(json => {
-              // this.buildImageGrid(json);
-              console.log(json);
+              this.buildImgGrid(json);
             });
   }
 
   // randomly shuffles an array in n time
-  static shuffle(arr) => {
+  static shuffle(arr) {
     for (let i = arr.length - 1; i >= 0; i--){
         const j = Math.floor(Math.random() * i);
         [arr[i], arr[j]] = [arr[j], arr[i]];
